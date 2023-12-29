227860956a8251f2130abd51cfbdbdecf9ba5720
ScaferuZ <endrafruz@gmail.com>
2023-12-29 10:13:45 +0700

    adding class for displaying all task

---
+++ zen_task0/src/Actions/DisplayTasks.java
@@ -0,0 +1,31 @@
+package Actions;
+
+import Todo.*;
+
+public class DisplayTasks extends Action {
+  @Override
+  public void showInformation() {
+    System.out.println("=----------------=");
+    System.out.println("= - Display Tasks - =");
+    System.out.println("=----------------=");
+  }
+
+  @Override 
+  public String userInput() {
+    throw new UnsupportedOperationException();
+  }
+
+  @Override
+  public void execute(String command) {
+    ZenTask.tasks.forEach((key, value) -> {
+      System.out.println("ID: " + key);
+      System.out.println("Title: " + value.getTitle());
+      System.out.println("Tags: " + value.getTags());
+      System.out.println("Description: " + value.getDescription());
+      System.out.println("Status: " + value.getStatus());
+      System.out.println("Project: " + value.getProjectName());
+      System.out.println("");
+    });
+      
+  }
+}
